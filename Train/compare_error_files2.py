53bd21ed322562b56c5fd0eacd1bb0851f538462
C:\Eduardo\Uacj\GitHub <sebastianxiro@gmail.com>
2019-01-22 12:08:52 -0700

    A little change

---
+++ Train/compare_error_files2.py
@@ -0,0 +1,3 @@
+"""
+Obtener la informacion
+"""
---
+++ UACJParkingLot/random_image.py
@@ -0,0 +1,11 @@
+import os
+
+def get_images_path():
+		path_images = 'C:\\Eduardo\\ProyectoFinal\\Pruebas_UACJ'
+		images_paths = []
+		for posible_dir in os.listdir(path_images):
+				posible_dir = os.path.join(path_images, posible_dir)
+				if os.path.isdir(posible_dir):
+						for filename in os.listdir(posible_dir):
+								images_paths.append(os.path.join(posible_dir, filename))
+		return images_paths
